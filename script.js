1fdbce5581ddcf875fd5f4a8107bf0e8cc2dd142
IgorSamuel06 <00001083968737sp@al.educacao.sp.gov.br>
2024-06-26 14:35:59 -0300

    Add files via upload
    Responsividade

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navbar = document.querySelector('.navbar');
+    burger.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+});
